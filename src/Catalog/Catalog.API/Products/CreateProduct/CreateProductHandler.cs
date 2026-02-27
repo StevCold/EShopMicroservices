cf9e6e23676e06e827af237ac51b5189b35d90c1
SteveCold <stefan_campan@yahoo.com>
2026-02-27 17:40:50 +0200

    added packages and started implementing CRUD

---
+++ src/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,36 @@
+﻿namespace Catalog.API.Products.CreateProduct;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
+    :ICommand<CreateProductResult>;
+public record CreateProductResult(Guid Id);
+
+internal class CreateProductComamandHandler(IDocumentSession session)
+    : ICommandHandler<CreateProductCommand, CreateProductResult>
+{
+    public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+    {
+        // Business logic to create a product
+        //save to database
+        //return CreateProductResult result
+        var product = new Product
+        {
+            Name = command.Name,
+            Category = command.Category,
+            Description = command.Description,
+            ImageFile = command.ImageFile,
+            Price = command.Price,
+        };
+        // TODO
+        //save to database
+        session.Store(product);
+        await session.SaveChangesAsync(cancellationToken);
+
+
+        //return result
+        return new CreateProductResult(product.Id);
+    }
+}
+
